0044a3e1f455404e765d2137325eaff67797857e
dioneyfroes <dioney.froes@hotmail.com>
2025-10-29 22:00:19 -0300

    estrutura

---
+++ src/config/appConfig.js
@@ -0,0 +1,8 @@
+/**
+ * CONFIGURAÇÃO CENTRALIZADA DA APLICAÇÃO
+ *
+ * Centraliza todas as configurações de forma segura e organizada.
+ * Separação entre configurações públicas e sensíveis.
+ */
+
+// Carregar variáveis de ambiente PRIMEIRO
---
+++ src/config/env.js
@@ -0,0 +1,4 @@
+/**
+ * Carregamento de variáveis de ambiente
+ * Este arquivo deve ser importado ANTES de qualquer outro módulo
+ */
