df84ca2575ea3ca375a764813b7e37ca97bf0933
dioneyfroes <dioney.froes@hotmail.com>
2025-11-29 16:19:56 -0300

    fix

--- src/infrastructure/api/dtos/OddsSchema.ts
+++ src/infrastructure/api/dtos/OddsSchema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 import { Odds } from '@core/odds/domain/value-objects/Odds';
 
 export const OddsSchema = z
-  .number({ required_error: 'Odds is required', invalid_type_error: 'Odds must be a number' })
+  .number()
   .min(Odds.MIN_VALUE, { message: `Odds must be greater than or equal to ${Odds.MIN_VALUE}` })
   .max(Odds.MAX_VALUE, { message: `Odds cannot be greater than ${Odds.MAX_VALUE}` });
 
